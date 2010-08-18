c65e2034e77bf8fc370774dc3bf30836ac3d6695
Russ Anderson <randerson@lastminutecom-labss-MacBook-Pro.local>
2010-08-18 23:57:23 +0100

    move to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bc2::Application.config.secret_token = '990a9788ba89281dc31aece0da4abb62ef98fc2b8ce3781806532f09a1d301183ab455de7dca9105ab8d1c7a4af4da74aea619f8ea4760027f7b31db86c2a2cf'
