9f114a99779dc0849dbf43d288663976fe6ed0d9
rux <russ@lastminute.com>
2010-09-15 23:53:37 +0100

    moved to jquery with submodule, now using jQuery lightBox plugin

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,10 @@ module Beatricecurnew
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    
+    
+    config.action_view.javascript_expansions[:defaults] = ['jquery-1.4.2', 'jquery-ujs/src/rails']
+    
   end
 end
