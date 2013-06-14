a64b9ab455230376d039021128c71e5796be5bbf
Russ Anderson <russ.anderson@yellglobal.com>
2013-06-14 14:19:06 +0100

    new images from scren grabs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,33 @@
 class PagesController < ApplicationController
-  
-  
-   
+
+
+
   def index
     @tab_highlight = "home"
   end
 
-  
+
   def contact
     @tab_highlight = "contact"
   end
-  
-  
+
+
   def gallery
     @tab_highlight = "gallery"
-    
+
     @images = [1,2,3,4,5,6,7]
-    
-   
+
+    @screen_images = [
+      "Beyond-Time_sad",
+      "Head-falling-onto-lap",
+      "Strawberry-to-mouth",
+      "Third-Falcon_leather-jacket",
+      "Under-Offer_angry-eyes"
+    ]
+
   end
 
-  
+
   def sound
     @tab_highlight = "sound"
   end
@@ -29,16 +36,16 @@ class PagesController < ApplicationController
     @tab_highlight = "video"
   end
 
-  
+
   def biog
     @tab_highlight = "biog"
   end
 
-  
+
   def news
     @tab_highlight = "news"
   end
-  
-  
- 
+
+
+
 end
